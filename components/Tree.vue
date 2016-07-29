0f27938cea4acd44cb470ab3f7a38c8fd6512e7a
liuyan29 <liuyan29@baidu.com>
2016-07-29 10:55:18 +0800

    add component-tree

---
+++ components/Tree.vue
@@ -0,0 +1,38 @@
+<template lang="html">
+  <ul>
+    <li v-for="item in items">
+      <div @click="toggle">
+        {{ item.name }}
+        <span class="glyphicon" :class="{'glyphicon-chevron-right':(!open && (item.children && item.children.length)),'glyphicon-chevron-down':(open && (item.children && item.children.length))}" aria-hidden="true"></span>
+      </div>
+      <vui-tree v-if="item.children" :items="item.children" v-show="open"></vui-tree>
+    </li>
+  </ul>
+</template>
+
+<script>
+export default {
+  name: 'vuiTree',
+  props: {
+    items: {
+      type: Array,
+      default() {
+        return []
+      }
+    }
+  },
+  data() {
+    return {
+      open: false
+    }
+  },
+  methods: {
+    toggle(e) {
+      this.open = !this.open
+    }
+  }
+}
+</script>
+
+<style lang="css">
+</style>
